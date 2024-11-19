7a78b8c81771b36d07637f6d972d37dfd52d6f1c
kashifkhanf1 <kashif.khan@visku.com>
2024-11-19 14:43:03 +0000

    feat: make file executable

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,3 +5,6 @@ echo "= Linting OAS file   ="
 echo "======================"
 
 cd /github/workspace/ && spectral lint "$INPUT_FILE_PATH"
+
+git add entrypoint.sh
+git update-index --chmod=+x entrypoint.sh
