50d9afdf17f6606e7be982b8c92c326f33b13c50
Ross <ross.mcdonald@sqrrlsolutions.com>
2025-07-07 11:38:35 +0100

    fb-1787 test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 echo "======================"
-echo "= Linting OAS file   ="
+echo "= Linting OAS sausage="
 echo "======================"
 
 cd /github/workspace/ && echo 'extends: ["spectral:oas"]' > .spectral.yaml && spectral lint "$INPUT_FILE_PATH"
